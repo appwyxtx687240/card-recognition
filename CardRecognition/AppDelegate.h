496a3f9565a7ab375b8086a3a7f68035b3205901
Summer <zhangjian@chinamobo.com>
2016-08-30 14:25:11 +0800

    项目创建

---
+++ CardRecognition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CardRecognition
+//
+//  Created by Summer on 16/8/30.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
